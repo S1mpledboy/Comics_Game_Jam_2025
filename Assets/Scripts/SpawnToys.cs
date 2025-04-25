1d9e3d5da4cc292142bf9692c42e3d49906f71b1
Pjoter9c <pduczek9c@wp.pl>
2025-04-25 21:03:37 +0200

    Foldery

--- Assets/Scripts/SpawnToys.cs
+++ Assets/Scripts/SpawnToys.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class XD : MonoBehaviour
+public class SpawnToys : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
