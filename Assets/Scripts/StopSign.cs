df7fe0a6966e0112c49e97329dec2a97cd43669e
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2025-04-26 00:29:57 +0300

    Collision added

--- Assets/Scripts/StopSign.cs
+++ Assets/Scripts/StopSign.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class StopSign : Sign
 {
+    CharacterController _player;
+    float _amountOfSlowDown;
     protected override void SignAbillity(CharacterController player)
     {
-        player.SetSpeed(player._speed * 0.7f);
-        gameObject.SetActive(false);
+        _player = player;
+        _amountOfSlowDown = _player.currentspeed * 0.3f;
+        print(_player.currentspeed);
+        _player.SetSpeed(_player.currentspeed - _amountOfSlowDown);
+        print(_player.currentspeed * 0.3f);
+        print(_player.currentspeed - _amountOfSlowDown);
+        print(_player.currentspeed);
+        Invoke(nameof(RenewPlayerSpeed), 3f);
+        transform.position = new Vector3(0,0,300f);
+        print("newspeed");
     }
+    void RenewPlayerSpeed()
+    {
+        
+        _player.SetSpeed(_player.currentspeed + _amountOfSlowDown);
+    }
+
 }
