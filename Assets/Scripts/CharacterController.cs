00fa2566ddc609e43e11e977e31cbf04d0632498
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2025-04-25 22:34:12 +0300

    Added Camera Movement and Player Movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterController : MonoBehaviour
+{
+    float _horizontalMovement, _verticalMovement;
+    [SerializeField] float _speed;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalMovement = Input.GetAxis("Horizontal");
+        _verticalMovement = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        if (_horizontalMovement == 0 && _verticalMovement == 0) return;
+        Vector3 directon = new Vector3(_horizontalMovement, _verticalMovement);
+        rigidbody.MovePosition(transform.position+directon*(_speed*Time.deltaTime));
+    }
+}
