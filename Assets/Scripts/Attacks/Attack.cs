6c8b07c9973c2abe82b7268ff5bca9ec00ba450e
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2025-04-26 17:29:52 +0300

    made animation for hearts

--- Assets/Scripts/Attacks/Attack.cs
+++ Assets/Scripts/Attacks/Attack.cs
@@ -57,7 +57,7 @@ public class Attack : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             if (collision.GetComponent<CharacterController>().isShielded) return;
-            CharacterController.OnTakeDamage?.Invoke(1);
+            CharacterController.OnTakeDamage?.Invoke(-1);
         }
     }
     protected virtual Vector2 CalculatePosition()
