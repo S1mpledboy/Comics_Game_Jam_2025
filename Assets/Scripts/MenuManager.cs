d3fe66a9cd5324944f5c2d46db69741b74d8f8a5
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2025-04-27 04:19:32 +0300

    Added Starting cut scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] PlayableDirector cutScene;
+    [SerializeField] GameObject cutSceneStaff;
+    public void StartCutScene()
+    {
+        cutSceneStaff.SetActive(true);
+        cutScene.Play();
+    }
+}
