5f67cc200873bed98d67ee905aca80b0667dc074
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2025-04-27 04:41:51 +0300

    hb

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
+
 using UnityEngine.Playables;
 public class MenuManager : MonoBehaviour
 {
@@ -11,20 +11,5 @@ public class MenuManager : MonoBehaviour
     {
         cutSceneStaff.SetActive(true);
         cutScene.Play();
-=======
-
-public class MenuManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
->>>>>>> Stashed changes
     }
 }
